9dea6618b9a5ba215421e2702b8d182c89677403
Pol Valletbó <pol.valletbo@glovoapp.com>
2023-04-19 15:53:34 +0200

    new project for telemetry

--- kotlin/TelemetrySystem/src/test/kotlin/TelemetryDiagnosticControlsTest.kt
+++ kotlin/TelemetrySystem/src/test/kotlin/TelemetryDiagnosticControlsTest.kt
@@ -1,11 +1,12 @@
 package tddmicroexercises.telemetrysystem
 
-import org.junit.Test
+import org.junit.jupiter.api.Test
 
 class TelemetryDiagnosticControlsTest {
 
     @Test
     fun CheckTransmission_should_send_a_diagnostic_message_and_receive_a_status_message_response() {
+        assert(false)
     }
 
 }
